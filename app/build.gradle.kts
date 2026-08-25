9eb2307eead07fe4b773f4c64e05e01e7752efd4
familiadbvtbo-web <familiadbvtbo@gmail.com>
2026-08-24 23:28:11 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,20 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+ id("com.android.application")
+ id("org.jetbrains.kotlin.android")
 }
-
 android {
-    namespace = "com.motorproprietario.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.motorproprietario.app"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 170
-        versionName = "V170"
-    }
+ namespace="com.motorproprietario.app"
+ compileSdk=35
+ defaultConfig {
+  applicationId="com.motorproprietario.app"
+  minSdk=26
+  targetSdk=35
+  versionCode=171
+  versionName="V171.0"
+ }
 }
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+ implementation("androidx.core:core-ktx:1.15.0")
+ implementation("androidx.appcompat:appcompat:1.7.0")
+ implementation("com.google.android.material:material:1.12.0")
 }
